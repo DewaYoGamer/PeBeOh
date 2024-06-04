1abd8c62353bbfca49e79011c0e90328494dfc63
dewayogamer <dewaanoc135@gmail.com>
2024-06-04 14:47:31 +0800

    change import

--- src/aplikasilowongankerja/handleMySQL.java
+++ src/aplikasilowongankerja/handleMySQL.java
@@ -1,17 +1,13 @@
 package aplikasilowongankerja;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class handleMySQL {
     public Connection connect() {
         Connection conn = null;
         try {
-            // Replace with your database url, username and password
             String url = "jdbc:mysql://localhost:3306/db_pbo";
             String user = "root";
             String password = "";
-
             conn = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
